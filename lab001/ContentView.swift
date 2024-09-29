809fe2177677bb5f6c8dbb87d60d358dff77c4d4
belharra64 <belharra64@gmail.com>
2024-09-29 22:02:36 +0200

    NewGrid

--- lab001/ContentView.swift
+++ lab001/ContentView.swift
@@ -13,61 +13,74 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            HStack {
-                ForEach(0..<cardCount, id:\.self) { index in
-                    CardView(content: emojis[index])
+            cards
+            cardCountAdjuster
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]) {
+            ForEach(0..<cardCount, id:\.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.medium)
+        .font(.largeTitle)
+    }
+    
+    func cartCountAdjuster(by offset: Int, emoji symbol: String ) -> some View {
+        Button(action: {
+                cardCount += offset
+           
+        }, label: {
+            Image(systemName: symbol)
+        } )
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    
+    var cardRemover: some View {
+        cartCountAdjuster(by: -1, emoji: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cartCountAdjuster(by: 1, emoji: "rectangle.stack.badge.plus.fill")
+    }
+}
+    
+    struct CardView: View {
+        let content: String
+        @State var isFaceUp = true
+        
+        
+        var body: some View {
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
+                if isFaceUp {
+                    base.fill(Color.white)
+                    base.strokeBorder(lineWidth: 5)
+                    Text(content).font(.largeTitle)
+                } else {
+                    base.fill(Color.orange)
                 }
             }
-            .foregroundColor(.orange)
-            
-            HStack {
-                Button(action: {
-                    if cardCount > 1 {
-                        cardCount -= 1
-                    }
-
-                }, label: {
-                    Image(systemName: "rectangle.stack.badge.minus.fill")
-                } )
-                
-                Spacer()
-                
-                Button(action: {
-                    if cardCount < emojis.count {
-                        cardCount += 1
-                    }
-                }, label: {
-                    Image(systemName: "rectangle.stack.badge.plus.fill")
-                } )
+            .onTapGesture {
+                isFaceUp.toggle()
             }
-            .imageScale(.medium)
-            .font(.largeTitle)
         }
-    }
-}
-
-struct CardView: View {
-    let content: String
-    @State var isFaceUp = true
-    
         
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
-                base.fill(Color.white)
-                base.strokeBorder(lineWidth: 5)
-                Text(content).font(.largeTitle)
-            } else {
-                base.fill(Color.orange)
-            }
-        }
-        .onTapGesture {
-            isFaceUp.toggle()
-        }
     }
-   
-}
+
 
 #Preview {
     ContentView()
